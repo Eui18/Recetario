671fa256d71d0634be77616cb09a7de66609cfc9
KiritoKazut0 <norbertolopez291@gmail.com>
2025-03-10 00:27:38 -0600

    implements list recet and view description for one recet

--- App.js
+++ App.js
@@ -1,94 +1,44 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
+import Login from "./src/Pages/Login"
+import Home from "./src/Pages/Home"
+import Descriptions from "./src/Pages/Descriptions";
+import {createStaticNavigation} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import ListRecet from "./src/Pages/ListRecet";
 
-const icon = require("./assets/rosa.jpg");
+
+const enrutador = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: {
+      screen: Home,
+      options: {
+        headerShown: false
+      }
+    },
+
+    ListRecet: {
+      screen: ListRecet
+    },
+
+    Descriptions: {
+      screen: Descriptions
+    }
+    
+  }
+})
+
+const Navegation = createStaticNavigation(enrutador);
 
 export default function App() {
-  const handleButtonPress = () => {
-    console.log("Botón presionado");
-  };
+ 
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <Image source={icon} style={styles.backgroundImage} />
-
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Sweeten</Text>
-        <Text style={styles.subtitle}>Sabores del mundo en tu cocina</Text>
-      </View>
-
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-          <Text style={styles.buttonText}>Get in now</Text>
-        </TouchableOpacity>
-      </View>
-
-      <StatusBar style="auto" />
-    </KeyboardAvoidingView>
+   <>
+      <Navegation></Navegation>
+   </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  backgroundImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-  },
-  titleContainer: {
-    position: "absolute",
-    top: "40%",
-    left: 0,
-    right: 0,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "#fff",
-    fontStyle: "italic",
-    textAlign: "center",
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#fff",
-    fontStyle: "italic",
-    textAlign: "center",
-    marginTop: 10,
-  },
-  bottomContainer: {
-    flex: 1,
-    backgroundColor: "#a11c55",
-    padding: 20,
-    justifyContent: "flex-end",
-  },
-  button: {
-    width: "100%",
-    height: 40,
-    backgroundColor: "#762146",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 300,
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-});
+
+
